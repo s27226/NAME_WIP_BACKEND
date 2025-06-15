6cc624d10a6dfca00ed175f911befc111b3b4e15
Jonasz <s27202@pjwstk.edu.pl>
2025-06-15 12:24:22 +0200

    dodano ProjectController

---
+++ projekt/backend/backend/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        public class LoginRequest
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] LoginRequest request)
+        {
+            if (request.Username == "admin" && request.Password == "password")
+            {
+                return Ok($"Witaj, {request.Username}");
+            }
+
+            return Unauthorized("Niepoprawne dane");
+        }
+    }
+    
+    
+}
---
+++ projekt/backend/backend/Controllers/ProjectsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("/[controller]")]
+public class ProjectsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetProjects()
+    {
+        var projects = new[]
+        {
+            new { id = 1, name = "Project 1", imageUrl = "" },
+            new { id = 2, name = "Project 2", imageUrl = "" }
+        };
+        return Ok(projects);
+    }
+}
