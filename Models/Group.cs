8ad8b472a18ab9b52c053325923bbd76af781613
Jonasz <s27202@pjwstk.edu.pl>
2025-10-22 18:22:39 +0200

    poprawki

--- Models/Group.cs
+++ Models/Group.cs
@@ -5,10 +5,5 @@ public class Group
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public string? Desc { get; set; }
-
-    public ICollection<UserGroup> UserGroups { get; set; } = new List<UserGroup>();
-    public ICollection<GroupInvitation> Invitations { get; set; } = new List<GroupInvitation>();
-    public ICollection<GroupRecommendation> Recommendations { get; set; } = new List<GroupRecommendation>();
-    public ICollection<Chat> Chats { get; set; } = new List<Chat>();
+    public ICollection<UserGroup> Members { get; set; } = new List<UserGroup>();
 }
-
